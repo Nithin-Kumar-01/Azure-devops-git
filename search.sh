002279a9464edb080f673704d9e44c2e2c72bfba
Nithin_Kumar_01 <test.gmail.com>
2024-09-10 17:42:41 +0530

    Dev-2

--- search.sh
+++ search.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+echo "This is the change"
 echo " Enter the Folder path you need to search"
 read path
 find $path -type f -name "*.txt" | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
